13e010dd8b9b7cf17ddcabb44bc4dbfbf7b50b94
Maciej <maciejweltrowski@gmail.com>
2024-06-05 18:46:50 +0200

    Initial commit

---
+++ src/main/java/com/maciew/whoIsThis/WhoIsThisApplication.java
@@ -0,0 +1,13 @@
+package com.maciew.whoIsThis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WhoIsThisApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WhoIsThisApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/maciew/whoIsThis/WhoIsThisApplicationTests.java
@@ -0,0 +1,13 @@
+package com.maciew.whoIsThis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WhoIsThisApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
